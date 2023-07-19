405d9eb3b0b428bdb49374e653f4790229a37d63
Roman Wind <119047529+RomanWind@users.noreply.github.com>
2023-07-19 14:05:49 +0200

    Collision detection added
    Player now detect collision with object and don't go thru kitchen counters that will be added in future

--- 3D Restaurant/Assets/Scripts/Player.cs
+++ 3D Restaurant/Assets/Scripts/Player.cs
@@ -11,7 +11,30 @@ public class Player : MonoBehaviour
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 movementDirection = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += movementDirection * movementSpeed * Time.deltaTime;
+
+        float movementDistance = movementSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, movementDirection, movementDistance);
+
+        if (!canMove)
+        {
+            Vector3 moveDirectionX = new Vector3(movementDirection.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionX, movementDistance);
+            if (canMove)
+                movementDirection = moveDirectionX;
+            else
+            {
+                Vector3 moveDirectionZ = new Vector3(0, 0, movementDirection.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionZ, movementDistance);
+                if(canMove)
+                    movementDirection = moveDirectionZ;
+            }
+        }
+
+        if (canMove)
+            transform.position += movementDirection * movementDistance;
+
         transform.forward = Vector3.Slerp(transform.forward, movementDirection, rotationSpeed * Time.deltaTime);
         isWalking = movementDirection != Vector3.zero;
     }
